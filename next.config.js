e9620b4ac35d8f610cc7e3c9bc15603ccc2aa35b
SubashD22 <101434737+SubashD22@users.noreply.github.com>
2023-01-23 22:17:50 +0530

    ignore lint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
+  images:{
+    domains:['res.cloudinary.com']
+  }
 }
 
 module.exports = nextConfig
