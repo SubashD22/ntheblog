df5d572e4273348532196dc5587665927413b374
SubashD22 <101434737+SubashD22@users.noreply.github.com>
2023-01-23 21:51:33 +0530

    second

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       Home
+      <Link href='/secondpg'>SecondPage</Link>
     </>
   )
 }
